77b1dc72b8ff5e220aadba8cae232aef3bd05254
김연호 <kimfine000@gmail.com>
2026-03-12 21:37:05 +0900

    Third Commit

--- src/main/java/org/example/attack/controller/OpenController.java
+++ src/main/java/org/example/attack/controller/OpenController.java
@@ -1,13 +1,16 @@
 package org.example.attack.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class OpenController {
 
     @GetMapping("/resource")
     public String getResource() {
+        log.info("api called");
         return "resource";
     }
 }
